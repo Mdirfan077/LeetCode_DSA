f517f2d234130ea7af7b139b5e496e0fe9de0839
Md Irfan <153820901+Mdirfan077@users.noreply.github.com>
2026-06-15 03:50:33 +0530

    Time: 292 ms (8.78%) | Memory: 110.4 MB (5.23%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        long mod = 1000000007L;
+        
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        // Previous Less Element
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+            
+            left[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+        
+        stack.clear();
+        
+        // Next Less or Equal Element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            
+            right[i] = stack.isEmpty() ? n - i : stack.peek() - i;
+            stack.push(i);
+        }
+        
+        long ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            long contribution = (long) arr[i] * left[i] * right[i];
+            ans = (ans + contribution) % mod;
+        }
+        
+        return (int) ans;
+    }
+}
