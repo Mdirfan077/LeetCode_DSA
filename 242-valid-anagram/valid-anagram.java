42b9f3c2cee4270b89044ab8ba1997addcf10ca6
Md Irfan <153820901+Mdirfan077@users.noreply.github.com>
2026-09-06 00:40:35 +0530

    Time: 4 ms (83.62%) | Memory: 44.4 MB (74.38%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int  s1=s.length();
+        int t1=t.length();
+        if(s1 != t1){
+            return false;
+        }
+
+        int []freq=new int [26];
+        for(int i=0; i<s1; i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+
+
+        }
+
+        for(int count: freq){
+            if(count !=0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
+
